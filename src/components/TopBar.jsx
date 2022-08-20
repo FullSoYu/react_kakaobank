6cf9980c00684192af9face719f5a5b69b386e29
FullSoYu <hey6410@naver.com>
2022-08-20 21:35:40 +0900

    재윤센세가 도와준 팀프로젝트 1차 아이템 대충 함

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -14,6 +14,24 @@ const TopBar = () => {
               />
             </a>
           </li>
+          <li className="nav-item">
+            <a href="#">은행소개</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">IR투자정보</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">상품안내</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">고객센터</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">이벤트</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">서류제출하기</a>
+          </li>
         </ul>
       </div>
     </div>
