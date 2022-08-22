fab03fc4d2c0dbe35e44210e03f15537571662ce
FullSoYu <hey6410@naver.com>
2022-08-23 03:15:19 +0900

    2차 메뉴 만드는 중

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -19,7 +19,28 @@ const TopBar = () => {
               은행소개
             </a>
             <div className="nav-subs">
-              <div className="nav-sub"></div>
+              <div className="nav-sub">
+                <div className="nav-sub-item">
+                  <a href="#" className="nav-sub-title">
+                    카카오뱅크
+                  </a>
+                </div>
+                <div className="nav-sub-item">
+                  <a href="#" className="nav-sub-title">
+                    윤리경영
+                  </a>
+                </div>
+                <div className="nav-sub-item">
+                  <a href="#" className="nav-sub-title">
+                    인재영입>
+                  </a>
+                </div>
+                <div className="nav-sub-item">
+                  <a href="#" className="nav-sub-title">
+                    새소식
+                  </a>
+                </div>
+              </div>
             </div>
           </li>
           <li className="nav-item">
