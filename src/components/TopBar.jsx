7b12a0c98e2dfd2ef7dca7831c19ef5207332ff7
FullSoYu <hey6410@naver.com>
2022-08-24 18:47:29 +0900

    입출금 통장 위치 확인

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -652,6 +652,7 @@ const TopBar = () => {
           </li>
         </ul>
       </div>
+      {/* 나눌영역 */}
       <div className="intro_main">
         <div className="intro_main_content">
           <h3 className="tit_main">
@@ -683,6 +684,33 @@ const TopBar = () => {
           </ul>
         </div>
         <div className="background-line"></div>
+      </div>
+      {/* 나눌 영역 */}
+      <div className="intro_cont cont_withdrawal">
+        <div className="cont_img">
+          <img
+            className="cont_img_img"
+            src="https://www.kakaobank.com/static/images/web/renewal/main-withdrawal.png"
+            alt="카카오뱅크 계좌 이체 화면"
+          />
+        </div>
+        <div className="cont_txt">
+          <h3 className="cont_txt_h3">
+            모바일로 더 손쉬운
+            <br />
+            계좌계설, 간편한 이체
+          </h3>
+          <p className="cont_txt_p">
+            인증서, OTP 없이 계좌 개설이 간편합니다.
+            <br />
+            여러건의 이체도 몇 번의 터치로 손쉽게 보낼 수 있습니다.
+          </p>
+        </div>
+        <div className="cont_btn">
+          <a className="link_bank" href="#">
+            카카오뱅크 입출금통장
+          </a>
+        </div>
       </div>
     </div>
   );
