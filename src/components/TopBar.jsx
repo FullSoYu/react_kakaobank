75b20aa9fe850d681624fb7dd2d1faeb77f2f28f
FullSoYu <hey6410@naver.com>
2022-08-26 18:47:39 +0900

    스크롤 구현전

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -720,7 +720,52 @@ const TopBar = () => {
       {/* 나눌영역 */}
       <div className="intro_cont_divider"></div>
       {/* 나눌영역 */}
-      <div className="intro_cont cont_deposit_and_savings"></div>
+      <div className="intro_cont cont_deposit_and_savings">
+        <div className="intro_cont_txt">
+          <h3 className="intro_cont_txt_h3">
+            우대조건 없이
+            <br />
+            합리적인 예금과 적금
+          </h3>
+          <p className="intro_cont_txt_p">
+            기대했던 금리와 다르게 복잡한 우대조건에 실망한 적이 있었나요?
+            <br />
+            카카오뱅크의 예금, 적금은 숨겨진 우대조건 없이 누구에게나 편리하고
+            합리적입니다.
+          </p>
+        </div>
+        <div className="intro_cont_btn">
+          <a className="link_deposit" href="#">
+            정기예금
+            <img
+              className="link_deposit_img"
+              src="https://www.kakaobank.com/static/images/web/home_arr.png"
+              alt=""
+            />
+          </a>
+          <a className="link_deposit" href="#">
+            자유적금
+            <img
+              className="link_deposit_img"
+              src="https://www.kakaobank.com/static/images/web/home_arr.png"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="intro_cont_img">
+          <img
+            className="intro_cont_img_img"
+            src="https://www.kakaobank.com/static/images/web/renewal/main-savings-money.png"
+            alt=""
+          />
+        </div>
+        <div className="intro_cont_img2">
+          <img
+            src="https://www.kakaobank.com/static/images/web/renewal/main-savings.png"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
