2345dfca30c86c63324d9c5f0b3bcdbaa44c024b
FullSoYu <hey6410@naver.com>
2022-08-29 14:51:54 +0900

    애니메이션 넣어야 됨

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -814,23 +814,59 @@ const TopBar = () => {
             <br />
             어느덧 만기 달성 경험을 맛보게 됩니다.
           </p>
-          <div className="intro_cont_5_btn">
-            <a href="#">
-              26주적금
-              <img
-                src="https://www.kakaobank.com/static/images/web/home_arr.png"
-                alt=""
-              />
-            </a>
-          </div>
-          <div className="intro_cont_5_img">
+        </div>
+        <div className="intro_cont_5_btn">
+          <a href="#">
+            26주적금
             <img
-              src="https://www.kakaobank.com/static/images/web/renewal/main-26weeks.png"
+              src="https://www.kakaobank.com/static/images/web/home_arr.png"
               alt=""
             />
-          </div>
+          </a>
+        </div>
+        <div className="intro_cont_5_img">
+          <img
+            src="https://www.kakaobank.com/static/images/web/renewal/main-26weeks.png"
+            alt=""
+          />
         </div>
       </div>
+      {/* 나눌영역 */}
+      <div className="intro_cont_divider"></div>
+      <div className="intro_cont_6">
+        <div className="intro_cont_6_img">
+          <img
+            src="https://www.kakaobank.com/static/images/web/renewal/main-foreignRemittance-new.png"
+            alt=""
+          />
+        </div>
+        <div className="intro_cont_6_txt">
+          <h3>
+            해외계좌송금과
+            <br />
+            WU빠른해외송금을
+            <br />더 쉽고, 저렴하게
+          </h3>
+          <p>
+            해외계좌송금이 가능한 22개국을 포함하여
+            <br />
+            전세계 200여개국으로
+            <br />
+            WU빠른해외송금이 가능합니다.
+          </p>
+        </div>
+        <div className="intro_cont_6_btn">
+          <a href="#">
+            해외송금
+            <img
+              src="https://www.kakaobank.com/static/images/web/home_arr.png"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      {/* 나눌영역 */}
+      <div className="intro_cont_7"></div>
     </div>
   );
 };
