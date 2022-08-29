bb16972204a0665a020af961a27a79b27129d538
FullSoYu <hey6410@naver.com>
2022-08-30 00:00:33 +0900

    컴포넌트 분활 완료

--- src/components/Intro_cont_7.jsx
+++ src/components/Intro_cont_7.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "../styles/Intro_cont_7.scss";
 
 const intro_cont_7 = () => {
-  return <div className="intro_cont_7_btn"></div>;
+  return (
+    <div>
+      <div></div>
+      <div className="intro_cont_7">
+        <div className="intro_cont_7_img">
+          <img
+            src="https://www.kakaobank.com/static/images/web/renewal/main-loan.png"
+            alt=""
+          />
+        </div>
+        <div className="intro_cont_7_txt">
+          <h3>복잡한 서류 제출을 간편하게, 내가 원하는 시간에</h3>
+          <p>
+            이사 날짜가 주말이나 공휴일이어도 전월세보증금 대출을 신청할수
+            있습니니다.
+          </p>
+        </div>
+        <div className="intro_cont_7_btn"></div>
+      </div>
+    </div>
+  );
 };
 
 export default intro_cont_7;
